bbee47899abf3f95ecd9d87009ff4c3cf2ca16e8
sjtu_lyric@outlook.com <sjtu_lyric@outlook.com>
2022-07-09 11:38:17 +0200

    needs json from app, "key":[]

--- report/views.py
+++ report/views.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 import json
-
+from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from report.models import Report
 import random
@@ -8,22 +8,21 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.views.generic import ListView
 
-
+@csrf_exempt
 def index(request):
+    # Report.objects.all().delete()
     if request.method == 'POST':
-        # TODO: not tested
-        key = request.POST.get('key')
-        imported_keys = ['k1', 'k2', 'k3', 'k4', 'k5']
-        # imported_keys = json.loads(request.body)['key']
-        if key:
+        # key = request.POST.get('key')
+        imported_keys = json.loads(request.body)["key"]
+        if imported_keys:
             objs = [Report(key=item) for item in imported_keys]
             Report.objects.bulk_create(objs)
-            return JsonResponse({ 'key': key }, status=201)
+            return JsonResponse({ 'postkeys': imported_keys },safe=False)
         return JsonResponse({ 'error': 'key not found in data' }, status=400)
 
     if request.method == 'GET':
         keys = Report.objects.values('key')
-        return JsonResponse({ 'keys': list(map(lambda x:x['key'], keys)) })
+        return JsonResponse({ 'getkeys': list(map(lambda x:x['key'], keys)) })
 
     return JsonResponse({ 'error': 'method not supported' }, status=405)
 
@@ -32,18 +31,3 @@ def fake(request):
     rand_key = random.randint(0, 2147483648)
     Report.objects.create(key=rand_key)
     return JsonResponse({ 'key': rand_key }, status=201)
-
-
-class test_post(ListView):
-    Report.objects.all().delete()
-
-    model = Report
-    template_name = 'test_index_post.html'
-    queryset = Report.objects.all()
-    # if not queryset.exists():
-    imported_keys = ['k1','k2','k3','k4','k5']
-    objs = [Report(key=item) for item in imported_keys]
-    Report.objects.bulk_create(objs)
-
-    def get_query_all(self):
-        return Report.objects.all()
