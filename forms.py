f1be756a24ca045251c5f76bab6f23baf605d23d
sjtu_lyric@outlook.com <sjtu_lyric@outlook.com>
2022-07-09 11:45:57 +0200

    needs json from app, {"key":[]}

--- forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class keyForm(forms.Form):
-    key = forms.CharField(label='key', max_length=256)
-    # created_at = forms.DateTimeField(label='time_stamp',auto_now_add=True)
